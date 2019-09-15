2f61c6d6de2401ccc0c77b51462f34d6f1b5ac00
alimadriz05 <54719667+alimadriz05@users.noreply.github.com>
2019-09-15 14:25:06 -0600

    aop y validaciones para Diego

---
+++ src/main/java/cr/ac/una/aop/MocionAOP.java
@@ -0,0 +1,67 @@
+package cr.ac.una.aop;
+
+import cr.ac.una.entities.Auditoria;
+import cr.ac.una.services.AuditoriaService;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Aspect
+public class MocionAOP {
+@Autowired
+AuditoriaService auditoriaService;
+
+//ASPECTO DESPUES DE CREAR
+    @After("execution(* cr.ac.una.services.MocionService.createMocion(..))")
+    public void auditoriaDespuesAgregarPersonas(JoinPoint joinPoint){//Funcion para loguear antes de algo
+      //Aca creo el objeto Auditoria y lo lleno
+        Auditoria a = new Auditoria();
+        a.setTransaccion("Agregado");
+        a.setTabla("MOCION");
+
+        auditoriaService.createAuditoria(a);
+        System.out.println("SE HA REGISTRADO LA TRANSACCION DE AGREGAR");
+    }
+
+    //ASPECTO DESPUES DE LISTAR
+    @After("execution(* cr.ac.una.services.MocionService.getAllMociones(..))")
+    public void auditoriaDespuesListarPersonas(JoinPoint joinPoint){
+        Auditoria a = new Auditoria();
+        a.setTransaccion("Listado");
+        a.setTabla("MOCION");
+
+        auditoriaService.createAuditoria(a);
+        System.out.println("SE HA REGISTRADO LA TRANSACCION DE LISTAR");
+    }
+
+    //ASPECTO DESPUES DE ELIMINAR
+    @After("execution(* cr.ac.una.services.MocionService.deleteMocion(..))")
+    public void auditoriaDespuesEliminarPersona(JoinPoint joinPoint){
+        Auditoria a = new Auditoria();
+        a.setTransaccion("Eliminado");
+        a.setTabla("MOCION");
+
+        auditoriaService.createAuditoria(a);
+        System.out.println("SE HA REGISTRADO LA TRANSACCION DE ELIMINAR");
+    }
+
+
+    //ASPECTO DESPUES DE ACTUALIZAR
+    @After("execution(* cr.ac.una.services.MocionService.updateMocion(..))")
+    public void auditoriaDespuesActualizarPersona(JoinPoint joinPoint){
+        Auditoria a = new Auditoria();
+        a.setTransaccion("Actualizado");
+        a.setTabla("MOCION");
+
+        auditoriaService.createAuditoria(a);
+        System.out.println("SE HA REGISTRADO LA TRANSACCION DE ACTUALIZAR");
+    }
+
+
+
+
+
+
+
+}
