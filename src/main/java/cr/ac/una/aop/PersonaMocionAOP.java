823704aa9c9f6be11d2e064ead2b0ae0ceed763b
alimadriz05 <54719667+alimadriz05@users.noreply.github.com>
2019-09-16 21:06:29 -0600

    confieeee

--- src/main/java/cr/ac/una/aop/PersonaMocionAOP.java
+++ src/main/java/cr/ac/una/aop/PersonaMocionAOP.java
@@ -8,48 +8,47 @@ import org.aspectj.lang.annotation.Aspect;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @Aspect
-public class MocionAOP {
-    @Autowired
-    AuditoriaService auditoriaService;
+public class PersonaMocionAOP {
+@Autowired
+AuditoriaService auditoriaService;
 
     //ASPECTO DESPUES DE CREAR
-    @After("execution(* cr.ac.una.services.MocionService.createMocion(..))")
-    public void auditoriaDespuesAgregarMociones(JoinPoint joinPoint){//Funcion para loguear antes de algo
+    @After("execution(* cr.ac.una.services.PersonaMocionService.createPersonaMocion(..))")
+    public void auditoriaDespuesAgregarPersonasMocion(JoinPoint joinPoint){//Funcion para loguear antes de algo
         //Aca creo el objeto Auditoria y lo lleno
         Auditoria a = new Auditoria();
         a.setTransaccion("Agregado");
-        a.setTabla("MOCION");
+        a.setTabla("PERSONA MOCION");
 
         auditoriaService.createAuditoria(a);
         System.out.println("SE HA REGISTRADO LA TRANSACCION DE AGREGAR");
     }
 
     //ASPECTO DESPUES DE LISTAR
-    @After("execution(* cr.ac.una.services.MocionService.getAllMociones(..))")
-    public void auditoriaDespuesListarMocioness(JoinPoint joinPoint){
+    @After("execution(* cr.ac.una.services.PersonaMocionService.getAllPersonaMocions(..))")
+    public void auditoriaDespuesListarPersonaMocion(JoinPoint joinPoint){
         Auditoria a = new Auditoria();
         a.setTransaccion("Listado");
-        a.setTabla("MOCION");
+        a.setTabla("PERSONA MOCION");
 
         auditoriaService.createAuditoria(a);
         System.out.println("SE HA REGISTRADO LA TRANSACCION DE LISTAR");
     }
 
     //ASPECTO DESPUES DE ELIMINAR
-    @After("execution(* cr.ac.una.services.MocionService.deleteMocion(..))")
-    public void auditoriaDespuesEliminarMocion(JoinPoint joinPoint){
+    @After("execution(* cr.ac.una.services.PersonaMocionService.deletePersonaMocion(..))")
+    public void auditoriaDespuesEliminarPersonaMocion(JoinPoint joinPoint){
         Auditoria a = new Auditoria();
         a.setTransaccion("Eliminado");
-        a.setTabla("MOCION");
+        a.setTabla("PERSONA MOCION");
 
         auditoriaService.createAuditoria(a);
         System.out.println("SE HA REGISTRADO LA TRANSACCION DE ELIMINAR");
     }
 
-
     //ASPECTO DESPUES DE ACTUALIZAR
-    @After("execution(* cr.ac.una.services.MocionService.updateMocion(..))")
-    public void auditoriaDespuesActualizarMocion(JoinPoint joinPoint){
+    @After("execution(* cr.ac.una.services.PersonaMocionService.updatePersonaMocion(..))")
+    public void auditoriaDespuesActualizarPersonaMocion(JoinPoint joinPoint){
         Auditoria a = new Auditoria();
         a.setTransaccion("Actualizado");
         a.setTabla("MOCION");
@@ -57,5 +56,24 @@ public class MocionAOP {
         auditoriaService.createAuditoria(a);
         System.out.println("SE HA REGISTRADO LA TRANSACCION DE ACTUALIZAR");
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
